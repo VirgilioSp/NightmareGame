512c96037d899779697e7f6770fdc4e03a244665
tinybunii <hbun0011@gmail.com>
2025-04-11 13:34:54 -0400

    Estrella enemiga

---
+++ Assets/scripts/starShooting.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class starShooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletPos;
+    private GameObject player;
+    private float timer;
+    private Animator animator;
+    private bool firstShot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        animator = GetComponent<Animator>();
+        firstShot = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 scale = transform.localScale;
+        if (player.transform.position.x < transform.position.x)
+        {
+            scale.x = Mathf.Abs(scale.x); 
+        }
+        else
+        {
+            scale.x = -Mathf.Abs(scale.x); 
+        }
+        transform.localScale = scale;
+
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+
+        if (distance < 15)
+        {
+            animator.SetBool("seesPlayer", true);
+
+            if (firstShot is false)
+            {
+                firstShot = true;
+            }
+            else
+            {
+                timer += Time.deltaTime;
+
+                if (timer > 1)
+                {
+                    animator.SetTrigger("attack");
+                    timer = 0;
+                    shoot();
+                }
+            }
+        }
+        else
+        {
+            animator.SetBool("seesPlayer", false);
+            timer = 0;
+        }
+            
+    }
+
+        void shoot()
+        {
+            Instantiate(bullet, bulletPos.position, Quaternion.identity);
+        }
+    }
